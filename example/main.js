daa9d6da548b67297f945ff98dd5c2bc0f2be227
GuckTube YT <56192597+GuckTubeYT@users.noreply.github.com>
2021-09-23 22:34:48 +0700

    Update main.js

--- example/main.js
+++ example/main.js
@@ -1,1 +1,9 @@
+const pconf = require("../index")
+const fs = require("fs")
 
+const config = fs.readFileSync("UserCustom.ini, "utf8")
+
+var decode = pconf.decode(config) //Decode UserCustom.ini File
+console.log("Decoded File = " + decode)
+var encode = pconf.encode(decode) //Encode
+fs.writeFileSync("encode.ini")
